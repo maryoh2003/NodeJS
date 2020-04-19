2397aa12a7361719f3172a055579c0b0805980b5
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-04-19 23:18:49 +0900

    [ADD] 익스프레스 웹 서버
    6장 익스프레스 웹 서버

--- learn-express/app.js
+++ learn-express/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,10 +24,10 @@ app.use(function(req, res, next){
 })
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //urlencoded는 주소 형식으로 데이터를 보내는 방식이다. false라면 노드의 querystring 모듈을 사용하여 쿼리스트링을 해석하고, true면 qs 모듈을 사용해 쿼리스트링을 해석한다
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -45,5 +47,17 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(cookieParser('secret code'));
+app.use(session({
+  resave: false,  //요청이 왔을 때 세션에 수정사항이 생기지 않더라도 세션을 다시 저장할지에 대한 설정
+  saveUninitialized: false, //세션에 저장할 내역이 없더라도 세션을 저장할지에 대한 설정
+  secret: 'secret code',  //필수 항목으로 cookie-parser의 비밀키와 같은 역할을 한다
+  cookie:{
+    httpOnly: true,
+    secure: false,
+  },
+}));
+app.use(flash());
 
 module.exports = app;
--- learn-express/routes/users.js
+++ learn-express/routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/flash', function(req, res){
+  req.session.message = '세션 메시지';
+  req.flash('message', 'flash 메시지'); //flash 메시지는 일회용이기 때문에 새로고침하면 없어짐
+  res.redirect('/users/flash/result');
+});
+
+router.get('/flash/result', function(req, res){
+  res.send(`${req.session.message} ${req.flash('message')}`);
+});
+
 module.exports = router;
